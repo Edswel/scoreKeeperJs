c68385ab145b67d7db4f48e3af84b1bf5fbd1072
Edswel <franckini@yahoo.com>
2022-12-05 16:03:58 +0100

    End increment when isGameOver is true

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 7;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
-  p1Score += 1;
-  p1Display.textContent = p1Score;
+  if (!isGameOver) {
+    p1Score += 1;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
 });
 
 p2Button.addEventListener('click', () => {
-  p2Score += 1;
-  p2Display.textContent = p2Score;
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
 });
