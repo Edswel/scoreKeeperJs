dcbbc7532c8426fbf400a803b76b296fd151d14c
Edswel <franckini@yahoo.com>
2022-12-07 17:49:23 +0100

    Refactor function to make it reuseable

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Button'),
+  display: p1Display = document.querySelector('#p1Display')
+}
+
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Button'),
+  display: p1Display = document.querySelector('#p2Display')
+}
+
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#play-to');
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', () => {
+function updateScores(player, opponent) {
   if (!isGameOver) {
-    p1Score += 1;
-    if (p1Score === winningScore) {
+    player.score += 1;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add('winner');
-      p2Display.classList.add('loser');
-      p1Button.disabled = true;
-      p2Button.disabled = true;
+      player.display.classList.add('winner');
+      opponent.display.classList.add('loser');
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    p1Display.textContent = p1Score;
+    player.display.textContent = player.score;
   }
+}
+
+p1.button.addEventListener('click', () => {
+  updateScores(p1, p2);
 });
 
-p2Button.addEventListener('click', () => {
-  if (!isGameOver) {
-    p2Score += 1;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p1Display.classList.add('loser');
-      p2Display.classList.add('winner');
-      p1Button.disabled = true;
-      p2Button.disabled = true;
-    }
-    p2Display.textContent = p2Score;
-  }
+p2.button.addEventListener('click', () => {
+  updateScores(p2, p1);
 });
 
 // Change event for winning score
